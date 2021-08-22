cf4f707ae2c1a7e3952f2fd313ab04b61b3fa37f
ROBYN MILLER <REM3191@HOTMAIL.COM>
2021-08-22 17:33:02 -0400

    updated hw

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,97 +1,39 @@
-// Function to determine marker size based on population
-function markerSize(population) {
-  return population / 40;
-}
+var earthquakesLayer=new L.LayerGroup();
 
-// An array containing all of the information needed to create city and state markers
-var locations = [
-  {
-    coordinates: [40.7128, -74.0059],
-    state: {
-      name: "New York State",
-      population: 19795791
-    },
-    city: {
-      name: "New York",
-      population: 8550405
-    }
-  },
-  {
-    coordinates: [34.0522, -118.2437],
-    state: {
-      name: "California",
-      population: 39250017
-    },
-    city: {
-      name: "Lost Angeles",
-      population: 3971883
-    }
-  },
-  {
-    coordinates: [41.8781, -87.6298],
-    state: {
-      name: "Michigan",
-      population: 9928300
-    },
-    city: {
-      name: "Chicago",
-      population: 2720546
-    }
-  },
-  {
-    coordinates: [29.7604, -95.3698],
-    state: {
-      name: "Texas",
-      population: 26960000
-    },
-    city: {
-      name: "Houston",
-      population: 2296224
-    }
-  },
-  {
-    coordinates: [41.2524, -95.9980],
-    state: {
-      name: "Nebraska",
-      population: 1882000
-    },
-    city: {
-      name: "Omaha",
-      population: 446599
-    }
+var earthquakes_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+d3.json(earthquakes_url, function (earthquakesdata) {
+  function generateColor(){
+    return "#fbff00"
   }
-];
+  function generateRadius(magnitude){
+    console.log(magnitude)
+    if (magnitude<1.0){
+      return 1
+    }
+    else{
+      return magnitude*10
+    }
+    
+  }
+  L.geoJson(earthquakesdata, {
+    pointToLayer: function (earthquake, coordinate) {
+      return L.circle(coordinate)
+    },
+    style: function (earthquake, coordinate) {
+      return {
+        stroke: true,
+        fillOpacity: 0.75,
+        color: generateColor(),
+        fillColor: generateColor(),
+        radius:generateRadius(earthquake.properties.mag)      }
+    },
+    onEachFeature: function (earthquake, earthquakeCircle) {
+      earthquakeCircle.bindPopup("pop successful")
+    }
+  }).addTo(earthquakesLayer)
+})
 
-// Define arrays to hold created city and state markers
-var cityMarkers = [];
-var stateMarkers = [];
 
-// Loop through locations and create city and state markers
-for (var i = 0; i < locations.length; i++) {
-  // Set the marker radius for the state by passing population into the markerSize function
-  stateMarkers.push(
-    L.circle(locations[i].coordinates, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: "white",
-      radius: markerSize(locations[i].state.population)
-    })
-  );
-
-  // Set the marker radius for the city by passing population into the markerSize function
-  cityMarkers.push(
-    L.circle(locations[i].coordinates, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: "purple",
-      fillColor: "purple",
-      radius: markerSize(locations[i].city.population)
-    })
-  );
-}
-
-// Create base layers
 
 // Streetmap Layer
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -110,23 +52,70 @@ var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z
   accessToken: API_KEY
 });
 
-// Create two separate layer groups below. One for city markers, and one for states markers
-var cityLayer=L.layerGroup(cityMarkers);
-var stateLayer=L.layerGroup(stateMarkers);
-
-// Create a baseMaps object to contain the streetmap and darkmap
-
-// Create an overlayMaps object here to contain the "State Population" and "City Population" layers
-var overlayMaps={
-"State Population":states,
-"City Population":cities
-}
-// Modify the map so that it will have the streetmap, states, and cities layers
-var myMap=L.map("map", {
-  center: [
-    37.09, -95.71
-  ],
-  zoom: 5,
+// Define a map object
+var myMap = L.map("map", {
+  center: [0,0],
+  zoom: 3,
+  layers: [darkmap, earthquakesLayer]
 });
+earthquakesLayer.addTo(myMap)
 
-// Create a layer control, containing our baseMaps and overlayMaps, and add them to the map
+// Create an overlay object
+var overlayMaps = {
+  "Earthquakes": earthquakesLayer,
+  
+};
+
+
+
+// Function to determine marker size based on population
+function markerSize(population) {
+  return population / 40;
+}
+
+// NEED TO ADD CODE WHERE MAP WILL GENERATE CITIBIKE DATA!!!
+
+// An array containing all of the information needed to create city and state markers
+
+// // Loop through locations and create city and state markers
+// for (var i = 0; i < earthquake.length; i++) {
+
+//   // Setting the marker radius for the state by passing population into the markerSize function
+//   stateMarkers.push(
+//     L.circle(locations[i].coordinates, {
+//       stroke: false,
+//       fillOpacity: 0.75,
+//       color: "white",
+//       fillColor: "white",
+//       radius: markerSize(locations[i].state.population)
+//     })
+//   );
+
+//   // Setting the marker radius for the city by passing population into the markerSize function
+//   cityMarkers.push(
+//     L.circle(locations[i].coordinates, {
+//       stroke: false,
+//       fillOpacity: 0.75,
+//       color: "purple",
+//       fillColor: "purple",
+//       radius: markerSize(locations[i].city.population)
+//     })
+//   );
+// }
+
+// Create base layers
+
+// Create two separate layer groups: one for cities and one for states
+// var states = L.layerGroup(stateMarkers);
+// var cities = L.layerGroup(cityMarkers);
+
+// Create a baseMaps object
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap
+};
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
